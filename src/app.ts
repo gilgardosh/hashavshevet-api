db5d566476f41b27b5f62d1d2065dcffbe291ad6
gilgardosh <gilgardosh@gmail.com>
2020-03-15 15:19:32 +0200

    error handling

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import http from 'http';
 import { applyMiddleware, applyRoutes } from "./utils";
-import routes from "./services";
 import middleware from "./middleware";
+import routes from "./services";
+import errorHandlers from "./middleware/errorHandlers";
 
+process.on("uncaughtException", e => {
+    console.log(e);
+    process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+    console.log(e);
+    process.exit(1);
+});
 
 const app: express.Application = express();
 applyMiddleware(middleware, app);
 applyRoutes(routes, app);
+applyMiddleware(errorHandlers, app);
 
 const server = http.createServer(app);
 const { PORT = 3000 } = process.env;
