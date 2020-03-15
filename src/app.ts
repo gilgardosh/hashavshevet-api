95e35c927bd2db8ecdcec3930fe0930e1364b113
gilgardosh <gilgardosh@gmail.com>
2020-03-15 14:54:48 +0200

    added routing functionality

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
 import express from 'express';
 import http from 'http';
-import { applyMiddleware } from "./utils";
+import { applyMiddleware, applyRoutes } from "./utils";
+import routes from "./services";
 import middleware from "./middleware";
 
 
 const app: express.Application = express();
 applyMiddleware(middleware, app);
+applyRoutes(routes, app);
 
 const server = http.createServer(app);
 const { PORT = 3000 } = process.env;
-
-app.use('/', (req, res) => {
-    res.send('hello');
-});
-
 
 server.listen(PORT, () =>
     console.log('Server is running http://localhost:' + PORT)
