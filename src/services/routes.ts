39d7bcf0acfb82a219f2ac5c04d09e9eb6ac10f1
gilgardosh <gilgardosh@gmail.com>
2020-03-16 17:35:53 +0200

    better routing, some UI

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,4 +1,20 @@
 import { Request, Response } from "express";
+import { napi,
+    tmpJurnalBatch,
+    chkJurnalBatch,
+    newJurnalBatch,
+    issueJurnalBatch,
+    importIndexRecords,
+    deleteIndexRecords,
+    importBankPageRecords,
+    exportDataRecords,
+    createDocument,
+    showDocument,
+    delDocument,
+    issueDoc,
+    crmActivities,
+    crmActivitiesTest,
+    CompaniesForToken } from "../utils/wizcloud";
 
 export default [
     // {
@@ -27,6 +43,133 @@ export default [
         method: "get",
         handler: async (req: Request, res: Response) => {
             res.sendFile(__dirname + '/ui/ab.html');
+        }
+    },
+    {
+        path: "/ad.html",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.sendFile(__dirname + '/ui/ad.html');
+        }
+    },
+    {
+        path: "/ba.html",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.sendFile(__dirname + '/ui/ba.html');
+        }
+    },
+    {
+        path: "/method/napi",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(napi());
+        }
+    },
+    {
+        path: "/method/tmpJurnalBatch",
+        method: "get",
+        handler: async (req: Request, res: Response) => {            
+            res.send(await tmpJurnalBatch(req.query));
+        }
+    },
+    {
+        path: "/method/chkJurnalBatch",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await chkJurnalBatch(req.query));
+        }
+    },
+    {
+        path: "/method/newJurnalBatch",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await newJurnalBatch());
+        }
+    },
+    {
+        path: "/method/issueJurnalBatch",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            console.log(req.query);
+            res.send(await issueJurnalBatch(req.query));
+        }
+    },
+    {
+        path: "/method/importIndexRecords",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await importIndexRecords(req.query));
+        }
+    },
+    {
+        path: "/method/deleteIndexRecords",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await deleteIndexRecords(req.query));
+        }
+    },
+    {
+        path: "/method/importBankPageRecords",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await importBankPageRecords(req.query));
+        }
+    },
+    {
+        path: "/method/exportDataRecords",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await exportDataRecords(req.query));
+        }
+    },
+    {
+        path: "/method/createDocument",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await createDocument(req.query));
+        }
+    },
+    {
+        path: "/method/showDocument",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await showDocument(req.query));
+        }
+    },
+    {
+        path: "/method/delDocument",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await delDocument(req.query));
+        }
+    },
+    {
+        path: "/method/issueDoc",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await issueDoc());
+        }
+    },
+    {
+        path: "/method/crmActivities",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await crmActivities(req.query));
+        }
+    },
+    {
+        path: "/method/crmActivitiesTest",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await crmActivitiesTest(req.query));
+        }
+    },
+    {
+        path: "/method/CompaniesForToken",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send(await CompaniesForToken());
         }
     }
 ];
