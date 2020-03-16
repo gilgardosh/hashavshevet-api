5f9b5e51980950628b7d0378f64b5033dc292e63
gilgardosh <gilgardosh@gmail.com>
2020-03-16 13:11:39 +0200

    part of UI

--- src/services/routes.ts
+++ src/services/routes.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from "express";
 
 export default [
+    // {
+    //     path: "/",
+    //     method: "get",
+    //     handler: async (req: Request, res: Response) => {
+    //         res.send("Hello world!");
+    //     }
+    // },
     {
         path: "/",
         method: "get",
         handler: async (req: Request, res: Response) => {
-            res.send("Hello world!");
+            res.sendFile(__dirname + '/ui/aa.html');
+        }
+    },
+    {
+        path: "/aa.html",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.sendFile(__dirname + '/ui/aa.html');
+        }
+    },
+    {
+        path: "/ab.html",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.sendFile(__dirname + '/ui/ab.html');
         }
     }
 ];
